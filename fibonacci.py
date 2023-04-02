3f8fab0e58c3ca0d72e7e8b9c9b191a93c983632
RimerMcquvue <anshul.229309232@muj.manipal.edu>
2023-04-03 00:38:28 +0530

    fibonacci using python

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,12 @@
-print("0\n1")
 a=0
-b=10
-n=input("enter the limit\n")
-i=0
+b=1
+n=int(input("enter the limit\n"))
+print("0\n1")
+i=2
 while i<n:
   x=a+b
   a=b
   b=x
-  print(x\n) 
+  print(x)  
   i+=1
+
