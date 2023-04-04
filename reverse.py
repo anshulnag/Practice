96828ac2e0ce9cbfde3a00aa29a05e2e7a6155b5
RimerMcquvue <anshul.229309232@muj.manipal.edu>
2023-04-04 15:54:07 +0530

    reverse

--- reverse.py
+++ reverse.py
@@ -1,4 +1,6 @@
 x=input("Enter a number\n")
+#long way
+'''
 y=len(x)
 x=int(x)
 i=a=b=0
@@ -7,4 +9,7 @@ while i<y:
     a=a*10+b
     x=int(x/10)
     i+=1
-print(a)    
+print(a)
+'''
+#simple way(can be used to reverse any text)
+print(x[::-1])
