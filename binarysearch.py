3d3e33875845703e82a70670ae6df8f211dc2fee
RimerMcquvue <anshul.229309232@muj.manipal.edu>
2023-07-06 22:06:38 +0530

    Binary Search

--- binarysearch.py
+++ binarysearch.py
@@ -1,32 +1,39 @@
 from array import *
-arr= array('i',[])
-n=int(input("Enter the size\n"))
-print("Enter the values in ascending order")
+
+arr = array("i", [])
+n = int(input("Enter the size\n"))
+print("Enter the values")
 for i in range(n):
-    x=int(input())
+    x = int(input())
     arr.append(x)
 print(arr)
-y=int(input("Enter value to be searched\n"))
-def search(arr,y):
-    start=0
-    end=len(arr)-1
-    while start<=end:
-        mid= int(start + (end-start)/2)
-        if y<arr[mid]:
-            end=mid-1
-        elif y==arr[mid]:
+y = int(input("Enter value to be searched\n"))
+
+
+def search(arr, y):
+    start = 0
+    end = len(arr) - 1
+    while start <= end:
+        mid = int(start + (end - start) / 2)
+        if y == arr[mid]:
             return mid
-        else:
-            start=mid+1
-    if start>end:
+        if arr[0] < arr[len(arr) - 1]:
+            if y < arr[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        if arr[0] > arr[len(arr) - 1]:
+            if y > arr[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+    if start > end:
         return -1
-    if arr[start]==y:
+    if arr[start] == y:
         return start
     else:
         return -1
 
-x=search(arr,y)
+
+x = search(arr, y)
 print(x)
-
-
-
